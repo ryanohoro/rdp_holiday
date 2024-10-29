b7a0a25a0c09b69fa68e7899b830dd380f6e7859
Will Metcalf <william.metcalf@gmail.com>
2024-10-29 16:47:48 -0500

    make it a proper installable thing..

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rdp_holiday",
+    version="0.1",
+    description="A Dumb RDP to JSON tool https://www.youtube.com/watch?v=ceR4TDuqE5A",
+    author="Will Metcalf",
+    author_email="william.metcalf@gmail.com",
+    url="https://github.com/wmetcalf/rdp_holiday", 
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+    entry_points={
+        "console_scripts": [
+            "rdp_holiday=rdp_holiday.rdp_holiday:main", 
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
